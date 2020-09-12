b10b051422e191fcb5360129b677f2e066260138
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-09-12 13:36:04 +0600

    feat: add select form & basic styles for it

--- components/FormElements/Select/index.tsx
+++ components/FormElements/Select/index.tsx
@@ -1,1 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select } from "antd";
+import { task, user } from "../../../data/data";
+
+type PropsSelect = {
+  menuType: any[];
+};
+
+export const SelectMenu: React.FC<PropsSelect> = ({ menuType }) => {
+  const { Option } = Select;
+
+  function onChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  function onBlur() {
+    console.log("blur");
+  }
+
+  function onFocus() {
+    console.log("focus");
+  }
+
+  function onSearch(val) {
+    console.log("search:", val);
+  }
+
+  return (
+    <Select
+      showSearch
+      style={{ width: 200 }}
+      // placeholder={selectName}
+      optionFilterProp="children"
+      onChange={onChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onSearch={onSearch}
+      filterOption={(input, option) =>
+        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      }
+    >
+      {menuType.map((name, key) => (
+        <Option value={key}>{name}</Option>
+      ))}
+    </Select>
+  );
+};
+
+export default SelectMenu;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
+import SelectMenu from "../components/FormElements/Select/index";
+import TaskForm from "../components/TaskForm";
+import TaskImplementationPercentage from "../constants/task-implementation-percentage";
+import { task, user } from "../data/data";
 
 const Home: React.FC = () => {
   return (
     <Layout>
       <h1>Hello World</h1>
+      <SelectMenu menuType={task.id}></SelectMenu>
+      <SelectMenu menuType={user.roles}></SelectMenu>
     </Layout>
   );
 };
