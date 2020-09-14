0cf3197d8aa7f844850233723c66d7b2c941aff0
Lena <lenach241@gmail.com>
2020-09-14 00:02:56 -0500

    feat: add logic to self-assessment form

---
+++ constants/requirement-implementation-options.ts
@@ -0,0 +1,7 @@
+const RequirementImplementationOptions = {
+  notImplemented: { name: "not implemented", value: 0 },
+  partlyImplemented: { name: "partly implemented", value: 0.5 },
+  fullyImplemented: { name: "fully implemented", value: 1 },
+};
+
+export default RequirementImplementationOptions;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
 
+const InnerComponent = () => {
+  return <div>Test</div>;
+};
+
 const Home: React.FC = () => {
   return (
     <Layout>
       <h1>Hello World</h1>
+      <div>
+        {() => {
+          return <div>Test</div>;
+        }}
+      </div>
+
+      <div>
+        <InnerComponent />
+      </div>
     </Layout>
   );
 };
