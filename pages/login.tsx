cc340d55791f53d8d9cbbd088cc608776d25af71
AnastasiyaGolunova <anastasiya.umka@gmail.com>
2020-09-17 17:04:57 +0300

    feat: add select role

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import SelectMenu from "../components/FormElements/Select";
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp({
@@ -34,8 +35,13 @@ class Login extends Component {
         <h2>Sing in to XCheck</h2>
         {this.state.isSignedIn ? (
           <span>
-            <div>Signed In!</div>
-            <h3>Welcome to XCheck, {firebase.auth().currentUser.email}</h3>
+            <h4>Welcome to XCheck, {firebase.auth().currentUser.email}</h4>
+            <h3>Choose your role</h3>
+            <>
+            <form>
+              <SelectMenu menuType={[]}/>
+            </form>
+            </>
             <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
 
           </span>
@@ -43,6 +49,7 @@ class Login extends Component {
           <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
         )}
       </div>
+
     );
   }
 }
